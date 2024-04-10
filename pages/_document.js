ab7f646096bbb8fdb1846ed997b4bdfb339dbd7e
brycem9 <123997306+brycem9@users.noreply.github.com>
2024-04-09 20:23:49 -0400

    favicon test

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document() {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+        <link rel="shortcut icon" href="/favicon/flat-gradient-social-media-icons-twitter-twitter-logo-png-clipart-thumbnail.jpg" type="image/x-icon" />
+          <link rel="icon" href="/favicon/flat-gradient-social-media-icons-twitter-twitter-logo-png-clipart-thumbnail.jpg" type="image/x-icon" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
-
